f0707955d313635a1e79038e308ee16741b751ad
nazmul53p <nazmul20018s@gmail.com>
2023-12-11 23:48:33 +0600

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,6 +62,7 @@ module.exports = {
         ],
         "no-unused-expressions": "off",
         "no-prototype-builtins": "off",
+
         // REACT
         "react/jsx-uses-react": "off",
         "react/react-in-jsx-scope": "off",
@@ -82,6 +83,19 @@ module.exports = {
         ],
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": 0,
+        "react/state-in-constructor": 0,
+        indent: 0,
+        "linebreak-style": 0,
+        "react/prop-types": 0,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "react/jsx-filename-extension": [
+            1,
+            {
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+        ],
+
+        "@typescript-eslint/explicit-module-boundary-types": "off",
 
         // Prettier
         // eslint looks for the prettier config at the top level of the package/app
