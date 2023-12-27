035c4f68e61a9003c6e91bee0a318b2eb446b9ff
raadu <raiyad.raad@gmail.com>
2023-12-27 15:15:56 +0600

    home page UI updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,15 @@ This page is rendered by default client side.  */
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
-            <h1 className="text-center text-6xl font-bold">
-                Welcome to{" "}
-                <a
-                    className="text-blue-600 hover:underline"
-                    href="https://sslwireless.com/"
-                >
-                    SSL
-                </a>
+        <main className="flex min-h-screen flex-col items-center bg-blue-100 p-24">
+            <h1 className="text-center text-5xl font-bold">
+                React Project Starter Framework
             </h1>
             <span className="pt-10">
                 <a
                     className="text-blue-600 hover:underline"
                     href="https://sslwireless.com/"
+                    target="_blank"
                 >
                     Documentation
                 </a>
@@ -25,8 +20,17 @@ export default function Home() {
                 <a
                     className="text-blue-600 hover:underline"
                     href="https://sslwireless.com/"
+                    target="_blank"
                 >
                     Repository
+                </a>
+                {" | "}
+                <a
+                    className="text-blue-600 hover:underline"
+                    href="https://sslwireless.com/"
+                    target="_blank"
+                >
+                    NPM Package
                 </a>
             </span>
         </main>
